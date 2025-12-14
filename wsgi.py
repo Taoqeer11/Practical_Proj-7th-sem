5cf6dc28e1e463f522420455d6ffef1c46be8915
digitalskillshouse <digitalskillshouse@gmail.com>
2025-12-14 14:38:30 +0500

    Initial clean commit (no secrets)

--- wsgi.py
+++ wsgi.py
@@ -3,4 +3,5 @@ from app import app
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port, debug=False)
+    print(f"Starting server on 0.0.0.0:{port}")
+    app.run(host="0.0.0.0", port=port, debug=False, threaded=True)
